e8880be578e75c4fb8be897613445b566cde3d80
Dulle99 <dule.sotirov99@gmail.com>
2026-04-24 23:01:39 +0200

    Add rule Update v2.1.1

--- monitoring-service/MonitoringService/Controllers/MonitoringRuleController.cs
+++ monitoring-service/MonitoringService/Controllers/MonitoringRuleController.cs
@@ -11,11 +11,13 @@ namespace MonitoringService.Controllers
     {
         private readonly MonitoringRuleState _ruleState;
         private readonly MonitoringRuleEngine _ruleEngine;
+        private readonly EdgeXCommandService _edgeXCommandService;
 
-        public MonitoringRuleController(MonitoringRuleState ruleState, MonitoringRuleEngine ruleEngine)
+        public MonitoringRuleController(MonitoringRuleState ruleState, MonitoringRuleEngine ruleEngine, EdgeXCommandService edgeXCommandService)
         {
             _ruleState = ruleState;
             _ruleEngine = ruleEngine;
+            _edgeXCommandService = edgeXCommandService;
         }
 
         [HttpGet]
@@ -70,6 +72,48 @@ namespace MonitoringService.Controllers
             {
                 message = "Monitoring rule reset to default values.",
                 rule = defaultRule
+            });
+        }
+
+        
+        [HttpPost("load-shed/on")]
+        public async Task<IActionResult> EnableLoadShed(CancellationToken cancellationToken)
+        {
+            var rule = _ruleState.GetRule();
+
+            await _edgeXCommandService.SendLoadShedCommandAsync(
+                rule.DeviceName,
+                true,
+                cancellationToken);
+
+            _ruleEngine.ResetCounter();
+
+            return Ok(new
+            {
+                message = "Load shedding enabled through EdgeX command.",
+                deviceName = rule.DeviceName,
+                loadShedEnabled = true
+            });
+        }
+
+        //For testing purposes, we can also have an endpoint to disable load shedding. In a real-world scenario, this might be triggered by a different event or condition.
+        [HttpPost("load-shed/off")]
+        public async Task<IActionResult> DisableLoadShed(CancellationToken cancellationToken)
+        {
+            var rule = _ruleState.GetRule();
+
+            await _edgeXCommandService.SendLoadShedCommandAsync(
+                rule.DeviceName,
+                false,
+                cancellationToken);
+
+            _ruleEngine.ResetCounter();
+
+            return Ok(new
+            {
+                message = "Load shedding disabled through EdgeX command.",
+                deviceName = rule.DeviceName,
+                loadShedEnabled = false
             });
         }
     }
